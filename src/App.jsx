ffc23521f76d7f520e371c6c9e4f5687e8dde802
Midoriya <victornascimentobr48@gmail.com>
2025-03-23 00:08:44 -0300

    Login-Form

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './Components/LoginSingnup';
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/LoginSingnup.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './LoginSingnup.css'; // Supondo que seu arquivo CSS se chama LoginSingnup.css
+
+import user_Icon from '../assets/user.png';
+import email_Icon from '../assets/email.png';
+import lock_Icon from '../assets/lock.png';
+
+const LoginSingnup = () => {
+
+  const [action,setaction] = useState("Sing Up")
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_Icon} alt="User Icon" className="icon" />
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input">
+          <img src={email_Icon} alt="Email Icon" className="icon" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <img src={lock_Icon} alt="Password Icon" className="icon" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+      <div class="forgot-password">Lost password? <span>Click Here!</span></div>
+      <div class="submit-container">
+  <div className={action==="login"?"submit gray":"submit"} onClick={()=>setaction("Sing Up")}>Sing Up</div>
+  <div className={action==="Sing Up"?"submit gray":"submit"} onClick={()=>setaction("login")}>login</div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSingnup;
